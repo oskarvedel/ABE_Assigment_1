6425cdef97b09ad393a4d02950a92631633b5803
Morten Dalsgaard <monglade.dalsgaard@gmail.com>
2021-02-19 11:56:55 +0100

    added admin controller functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,3 +21,49 @@ module.exports.CreateAdmin = async function (req, res) {
     };
 };
 
+//Returns a list of admins
+module.exports.GetAllAdmins = async function (req, res) {
+    const admins = await adminCollection.find({})
+        .catch(reason =>
+            res.status(400).json({
+                "title": "Unable to find any admins from the database",
+                "detail": reason
+            })
+        );
+    res.status(200).json({
+        admins
+    })
+};
+
+//Gets a specific admin
+module.exports.getAdminById = async function (req, res) {
+    try {
+        const admin = await adminCollection.findById(req.params.adminid);
+        if (admin) {
+            res.status(200).json({
+                admin
+            })
+        } else {
+            throw ("Admin not found");
+        }
+    } catch (err) {
+        res.status(400).json({
+            "title": "Unable to find admin from the database",
+            "detail": err
+        })
+    };
+}
+
+//Deletes a specific admin
+module.exports.DeleteAdminById = async function (req, res) {
+    try {
+        //Need to add real admin id
+        await adminCollection.findByIdAndDelete(req.params.adminid);
+        res.status(200).send();
+    } catch (err) {
+        res.status(400).json({
+            "title": "Unable to delete admin from the database",
+            "detail": err
+        })
+    };
+}
