36df4f95cc5d470083c63f21c5b67dcbd111f2d7
Morten Dalsgaard <monglade.dalsgaard@gmail.com>
2021-02-26 10:51:51 +0100

    added jwt and roles for admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const adminController = require('../controllers/admin');
-
+const authorize = require('../helpers/authorize')
+const jwt = require('express-jwt');
+const auth = jwt({
+    secret: process.env.JWT_SECRET,
+    requestProperty: 'payload'
+});
 
 /**
  * @swagger
@@ -10,11 +15,11 @@ const adminController = require('../controllers/admin');
  *     summary: test
  *     description: test
 */
-router.route('').get(adminController.GetAllAdmins)
-        .post(adminController.CreateAdmin);
+router.route('').get(authorize(Role.Admin), auth, adminController.GetAllAdmins)
+        .post(authorize(Role.Admin), auth, adminController.CreateAdmin);
 
 router.route('/:adminid')
-    .get(adminController.getAdminById)
-    .delete(adminController.DeleteAdminById)
+    .get(authorize(Role.Admin), auth, adminController.getAdminById)
+    .delete(authorize(Role.Admin), auth, adminController.DeleteAdminById)
 
-module.exports = router;
+module.exports = router;    
