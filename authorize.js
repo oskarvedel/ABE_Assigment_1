375e14bb94859970ed07197db57a76d7a4a8a56e
SBrahe <201807173@post.au.dk>
2021-02-26 10:56:59 +0100

    authorize

---
+++ authorize.js
@@ -0,0 +1,25 @@
+const jwt = require('express-jwt');
+const { secret } = require('config.json');
+
+module.exports = authorize;
+
+function authorize(roles = []) {
+    if (typeof roles === 'string') {
+        roles = [roles];
+    }
+
+    return [
+        // authenticate JWT token and attach user to request object (req.user)
+        jwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'] }),
+
+        // authorize based on user role
+        (req, res, next) => {
+            if (roles.length && !roles.includes(req.user.role)) {
+                // user's role is not authorized
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+            // authentication and authorization successful
+            next();
+        }
+    ];
+}
