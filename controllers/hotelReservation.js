8cdf6070828cc79d9a7c7549ea37736918e962d9
oskarvedel <oskar.vedel@gmail.com>
2021-03-02 19:05:24 +0100

    it works

--- controllers/hotelReservation.js
+++ controllers/hotelReservation.js
@@ -4,24 +4,19 @@ const saltRounds = 10;
 const hotelCollection = require('../model/hotel');
 const hotel = require('../model/hotel');
 
+//Create an Hotel
 module.exports.CreateHotel = async function (req, res) {
     let hotel = await hotelCollection.create({
-        name: req.body.hotelname,
+        name: req.body.hotelname
     }).catch(reason =>
         res.status(400).json({
-            "title": "Unable to create an Admin",
+            "title": "Unable to create an hotel",
             "detail": reason
         })
     );
-    if (hotel)
         res.status(201).json({
             hotel
-        })
-    else {
-        res.status(500).json({
-            "title": "ETERNAL SERVER ERROR"
-        })
-    };
+        });
 };
 
 //Get all hotels
@@ -36,22 +31,6 @@ module.exports.GetAllHotels = async function (req, res) {
     res.status(200).json({
         hotels
     })
-};
-
-//Create an Hotel
-module.exports.CreateHotel = async function (req, res) {
-    let hotel = await hotelCollection.create({
-        name: req.body.hotelname
-    }).catch(reason =>
-        res.status(400).json({
-            "title": "Unable to create an hotel",
-            "detail": reason
-        })
-    );
-        res.status(201).json({
-            hotel
-        });
-        
 };
 
 //Returns a list of admins
