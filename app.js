53a3d0dae68f8c0383b7f831ef03274642545d51
SBrahe <201807173@post.au.dk>
2021-03-02 15:00:07 +0100

    route added

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
+var guestRouter = require(' ./routes/guest')
 
 //swagger setup
 var swaggerUi = require('swagger-ui-express');
@@ -49,6 +50,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admins',adminRouter);
+app.use('/guests',guestRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/guest.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const adminController = require('../controllers/guest');
+const authorize = require('../authorize')
+const jwt = require('express-jwt');
+const Role = require('../roles');
+
+const auth = jwt({
+    secret: process.env.JWT_SECRET,
+    requestProperty: 'payload',
+    algorithms: ['HS256']
+});
+
+/**
+ * @swagger
+ * /guest:
+ *   get:
+ *     summary: test
+ *     description: test
+*/
+router.route('').get(authorize(Role.Guest), auth, guestController.GetAllGuests)
+        .post(authorize(Role.Guest), auth, guestController.CreateGuest);
+
+router.route('/:guestid')
+    .get(authorize(Role.Guest), auth, guestController.getGuestById)
+    .delete(authorize(Role.Guest), auth, guestController.DeleteGuestById)
+
+module.exports = router;    
