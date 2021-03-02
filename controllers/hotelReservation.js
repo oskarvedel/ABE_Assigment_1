691dcb47708591429be86b2a5028048e040f7ea6
oskarvedel <oskar.vedel@gmail.com>
2021-03-02 20:17:20 +0100

    cleanup

--- controllers/hotelReservation.js
+++ controllers/hotelReservation.js
@@ -33,53 +33,6 @@ module.exports.GetAllHotels = async function (req, res) {
     })
 };
 
-//Returns a list of admins
-module.exports.GetAllAdmins = async function (req, res) {
-    let admins = await userCollection.find({})
-        .catch(reason =>
-            res.status(400).json({
-                "title": "Unable to find any admins from the database",
-                "detail": reason
-            })
-        );
-    res.status(200).json({
-        admins
-    })
-};
-
-//Gets a specific admin
-module.exports.getAdminById = async function (req, res) {
-    try {
-        const admin = await userCollection.findById(req.params.adminid);
-        if (admin) {
-            res.status(200).json({
-                admin
-            })
-        } else {
-            throw ("Admin not found");
-        }
-    } catch (err) {
-        res.status(400).json({
-            "title": "Unable to find admin from the database",
-            "detail": err
-        })
-    };
-}
-
-//Deletes a specific admin
-module.exports.DeleteAdminById = async function (req, res) {
-    try {
-        //Need to add real admin id
-        await userCollection.findByIdAndDelete(req.params.adminid);
-        res.status(200).send();
-    } catch (err) {
-        res.status(400).json({
-            "title": "Unable to delete admin from the database",
-            "detail": err
-        })
-    };
-}
-
 //Signs up a new user
 module.exports.SignUp = async function (req, res) {
     try {
