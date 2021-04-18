a817d69d272b5f1383b4dbefbe24f7be6a0c26c4
oskarvedel <oskar.vedel@gmail.com>
2021-04-18 15:36:25 +0200

    fixed swagger per pouls feedback

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require ('dotenv').config();
+require('dotenv').config();
 
 var initializeDB = require("./model/initializeDB");
 
@@ -56,12 +56,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
